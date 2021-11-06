85bc7b922a1cc6225701e63d1fa85eacebfb601f
skyupguf <skyupguf@gmail.com>
2021-11-06 23:51:49 +0900

    Add Lv2 code

--- programmers/Lv.2/교점에_별_만들기.js
+++ programmers/Lv.2/교점에_별_만들기.js
@@ -3,14 +3,13 @@
 //  2. 길이가 2 ~ 1,000 사이인 2차원 배열인 line의 원소는 [A, B, C] 형태로 존재한다.
 //  3. A, B, C는 -100,000 이상 100,000 이하인 정수로 무수히 많은 교점이 생기는 직선 쌍은 주어지지 않는다. ex) A=0, B=0
 //  4. 직선의 교점을 구하는 방법은 아래와 같다.
-//      - Ax + By + E = 0, Cx + Dy + F = 0 일 경우
-//      - x = (BF-ED) / (AD-BC), y = (EC-AF) / (AD-BC) (x, y)의 좌표다. AD - BC = 0인 경우 두 직선은 평행 또는 일치한다.
+//      - i0x + i1y + i2 = 0, j0x + j1y + j2 = 0 일 경우가 교점좌표이며 i0j1 - i1j0 = 0인 경우 두 직선은 평행 또는 일치한다.
+//      - x = (i1j2-i2j1) / (i0j1-i1j0), y = (i2j0-i0j2) / (i0j1-i1j0) (x, y)의 좌표다. i0j1-i1j0면 NaN, -infinity
 //      - line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]] 일 경우
-//      - 2x - y + 4 = 0, -2x - y + 4 = 0, -y + 1 = 0, 5x - 8y - 12 = 0, 5x + 8y + 12 = 0
-//      - [2, -1, 4], [-2, -1, 4] => 2x - y + 4 = 0, -2x - y + 4 = 0 
-//      - x = (line[0][1] * line[1][2] - line[0][2] * line[1][1]) / (line[0][0] * line[1][1] - line[0][1] * line[1][0])
-//      - y = (line[0][1] * line[1][2] - line[0][2] * line[1][1]) / (line[0][0] * line[1][1] - line[0][1] * line[1][0])
-//      - 정수 좌표들만 계산하면 (4, 1), (4, -4), (-4, -4), (-4, 1), (0, 4)
+//      - [2, -1, 4], [-2, -1, 4] => a0b1 - a1b0 = line[i][0]*line[j][1] - line[i][1]*line[j][0]
+//      - x = (line[i][1]*line[j][2] - line[i][2]*line[j][1]) / a0b1 - a1b0
+//      - y = (line[i][2]*line[j][0] - line[i][0]*line[j][2]) / a0b1 - a1b0
+//      - 정수 좌표들만 계산하면 (4, 1), (4, -4), (-4, -4), (-4, 1), (0, 4) (-1, -2)  4 -4  9  /  4 -4  9
 //  5. 교점이 되는 부분을 *, 빈 공간을 . 이라 하면 위 좌표의 결과는 아래와 같다.
 //        [                     [
 //          "..........."           "....*....",  
@@ -31,7 +30,33 @@
 
 //  코드
 const findIntersectionPoint = (line) => {
+    const coordinates = [];
+    let maxR = -100000, minR = 100000, distR = 0, r0Index;
+    let maxC = -100000, minC = 100000, distC = 0, c0Index;
 
+    for(let i=0; i<line.length; i++) for(let j=i+1; j<line.length; j++) {
+        let denominator = line[i][0]*line[j][1] - line[i][1]*line[j][0];
+        if(!denominator) continue;
+
+        let x = (line[i][1]*line[j][2] - line[i][2]*line[j][1]) / denominator;
+        let y = (line[i][2]*line[j][0] - line[i][0]*line[j][2]) / denominator;
+        if(x - parseInt(x) || y - parseInt(y)) continue;
+        
+        maxR = Math.max(maxR, y), minR = Math.min(minR, y);
+        maxC = Math.max(maxC, x), minC = Math.min(minC, x);
+        coordinates.push([x, y]);
+    }
+    distR = maxR - minR, distC = maxC - minC;
+    r0Index = distR - maxR, c0Index = distC - maxC;
+
+    const points = new Array(distR+1).fill('.'.repeat(distC+1));
+    coordinates.forEach(([a, b]) => {
+        let x = a < 0 ? c0Index + a :  c0Index - a;
+        let y = b < 0 ? r0Index + b :  r0Index - b;
+        let pointedRow = points[y].split('');
+        pointedRow[x] = '*', points[y] = pointedRow.join('');
+    });
+    return points;
 }
 /*
 풀이
@@ -39,4 +64,11 @@ const findIntersectionPoint = (line) => {
 2. 좌표값은 배열로 저장하고 각 배열에서 가장 높은 x, y값과 가장 작은 x, y값을 추출한다.
 3. 두 값중 양과 음의 정수로 나뉘었을 경우 x+y+1의 길이만큼 행렬을 만들어야 한다.
 4. 만들어진 행렬의 좌표에 *로 대체하고 리턴한다.
+5. 음수의 좌표와 양수의 좌표를 어떻게 배열안의 문자열로 넣을 것인가가 관건
+6. row[1]이 0보다 작을 경우 어디가
+
+에러핸들링
+1. 분모가 0이 올 경우 정의되지 않은 수로 NaN 혹은 -Infinity를 반환하고 배열을 생성하지 못한다.
+2. 분모를 먼저 구하고 0인 경우 continue로 다음 수를 루프한다.
+3. row와 col값을 바꿔서 할당함, x가 col y가 row다.
 */
