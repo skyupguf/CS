1ed425bfaff675fb8a61741ad8f817af49efc16c
skyupguf <skyupguf@gmail.com>
2021-11-29 02:35:05 +0900

    Refact Binary_search_tree

--- Basic/[Tree]Binary_search_tree.js
+++ Basic/[Tree]Binary_search_tree.js
@@ -13,8 +13,8 @@
 class BinarySearchTree {
     constructor(data) {
         this.value = data;
-        this.left;
-        this.right;
+        this.left = null;
+        this.right = null;
     }
 
     addNode(data) {
@@ -42,55 +42,52 @@ class BinarySearchTree {
 
     deleteNode(root, data) {
         if (data < root.value) {
-            root.left = root.left.deleteNode(data);
+            root.left = this.deleteNode(root.left, data);
         
-        } else if (data > this.value) {
-            this.right = this.right.deleteNode(data);
+        } else if (data > root.value) {
+            root.right = this.deleteNode(root.right, data);
         
         } else {
-            if (!this.left) return this.right;
-            else if (!this.right) return this.left;
+            if (!root.left) return root.right;
+            else if (!root.right) return root.left;
             else {
-                let min = this.right;
+                let min = root.right;
                 while (min.left) min = min.left;
-                [ this.value, min.value ] = [ min.value, this.value ];
-                this.right = this.right.deleteNode(data);
+                [ root.value, min.value ] = [ min.value, root.value ];
+                root.right = this.deleteNode(root.right, data);
             }
         }
         return root;
     }
     
     preorder(callback) {
-        const root = this.root;
-        callback(root.value);
-        if (root.left) root.left.preorder(callback);
-        if (root.right) root.right.preorder(callback);
+        callback(this.value);
+        if (this.left) this.left.preorder(callback);
+        if (this.right) this.right.preorder(callback);
     }
   
     inorder(callback) {
-        const root = this.root;
-        if (root.left) root.left.inorder(callback);
-        callback(root.value);
-        if (root.right) root.right.inorder(callback);
+        if (this.left) this.left.inorder(callback);
+        callback(this.value);
+        if (this.right) this.right.inorder(callback);
     }
   
     postorder(callback) {
-        const root = this.root;
-        if (root.left) root.left.postorder(callback);
-        if (root.right) root.right.postorder(callback);
-        callback(root.value);
+        if (this.left) this.left.postorder(callback);
+        if (this.right) this.right.postorder(callback);
+        callback(this.value);
     }
 }
 /*
     풀이
     1. 노드의 원소를 할당할 value, 왼쪽 하위트리 left, 오른쪽 하위트리 right를 멤버변수로 선언한다.
     
-    2. 노드를 추가하는 메서드 addChildNode를 구현한다.
+    2. 노드를 추가하는 메서드 addNode를 구현한다.
         2-1. 현재 루트노드인 value와 비교해서 작을 경우 left로 이동한다.
         2-2. left가 null이 아닐 경우, 다시 left, right를 판단해야 하므로 재귀호출한다.
         2-3. left가 null이면 data값을 인자로 전달한 인스턴스를 할당한다.
         2-4. right도 위와 동일한 로직으로 구현한다.
-        2-5. 만일 노드와 값이 동일하거나 수가 아닐 경우 에러처리 한다.
+        2-5. 만일 노드와 값이 동일하거나 수가 아닐 경우, undefined를 return한다.
     
     3. 노드를 탐색할 메서드 checkNode를 구현한다.
         3-1. 현재 노드의 value와 data가 같으면 true를 리턴한다.
@@ -99,8 +96,12 @@ class BinarySearchTree {
         3-4. 일치하는 원소가 없을 경우 false를 리턴한다.
     
     4. 노드를 삭제할 메서드 deleteNode를 구현한다.
-        4-1. 노드가 삭제되는 경우의 수는 4가지, left만 존재하는 경우, right만 존재하는 경우, 둘 다 있는 경우, 둘 다 없는 경우
-        4-2. 우선 left나 right 둘 중 하나의 자식만 존재하는 경우 노드를 제거하고 left 또는 right를 끌어 올린다.
+        4-1. 노드는 자식노드가 left 또는 right 하나만 존재하는 경우, 둘 다 있는 경우, 둘 다 없는 경우로 나뉜다.
+        4-2. left만 존재할 경우 삭제할 노드의 포인터에 left를 할당해 메모리에서 제거한다.
+        4-3. right만 존재할 경우 삭제할 노드의 포인터에 right를 할당해 메모리에서 제거한다.
+        4-4. 둘 다 없는 경우는 단말노드이기 때문에 left, right가 모두 null이라 위 두 조건에서 자동으로 설정된다.
+        4-5. 둘 다 존재하는 경우 left에서 가장 큰 값, right에서 가장 작은 값과 삭제할 노드의 value를 교환한다.
+        4-6. 이제 삭제할 노드가 단말에 위치하므로 선택한 하위트리로 재귀호출하면 4-4로 처리된다.
     
     5. 전위순회를 구현한다.
         5-1. callback으로 노드를 출력할 함수를 전달 받는다.
@@ -115,88 +116,11 @@ class BinarySearchTree {
         6-4. 후위는 left => right => root 순으로 원소를 callback함수에 전달한다.
     
     에러핸들링
-    1. checkNode에서 존재하지 않는 노드의 원소를 찾을 때 null값을 만나는 경우 null.포인터로 재귀호출 불가
-    2. 하위트리 분기에서 left와 right가 둘다 존재하는지 && 조건으로 추가한다.
+    1. deleteNode()의 재귀호출의 경우 포인터 할당과 리턴위치 설정에 유의해야 한다.
+        1-1. 우선 재귀호출로 탐색중인 노드 삭제시 두 개의 if문에서 return되는 root.right와 root.left를 할당시켜야 한다.
+        1-2. 그리고 삭제되지 않은 노드의 경우 해당 root를 다시 return으로 할당 받으면 된다.
+    
+    시간복잡도
+    1. 노드의 추가 경우, 탐색의 경우 만일 트리가 편향되어 있다면 최악으로 O(N)의 시간복잡도가 소요될 수 있다.
+    2. 노드의 삭제 경우, 최대 단말노드까지 탐색이므로 최악의 경우 계층의 높이인 O(H)까지 탐색된다.
 */
-class Node {
-    constructor(data) {
-        this.value = data;
-        this.left;
-        this.right;
-    }
-}
-
-class BinarySearchTree {
-    constructor(data) {
-        this.root = new Node(data);
-    }
-
-    addNode(data) {
-        const root = this.root;
-        
-        if (!root.value) root = new BinarySearchTree(data);
-
-        if (data < root.value) {
-            root.left 
-            ? root.left.addNode(data) 
-            : root.left = new BinarySearchTree(data);
-        
-        } else if (data > root.value) {
-            root.right 
-            ? root.right.addNode(data) 
-            : root.right = new BinarySearchTree(data);
-            
-        } else return;
-    }
-    
-    checkNode(data) {
-        const root = this.root;
-        if (data === root.value) return true;
-        else if (data < root.value && root.left) return root.left.checkNode(data);
-        else if (data > root.value && root.right) return root.right.checkNode(data);
-        else return false;
-    }
-
-    deleteNode(data) {
-        const root = this.root;
-
-        if (data < root.value) {
-            root.left.root = root.left.deleteNode(data);
-        
-        } else if (data > root.value) {
-            root.right.root = root.right.deleteNode(data);
-        
-        } else {
-            if (!root.left) return root.right;
-            else if (!root.right) return root.left;
-            else {
-                let min = root.right.root;
-                while (min.left) min = min.left.root;
-                [ root.value, min.value ] = [ min.value, root.value ];
-                root.right = root.right.deleteNode(data);
-            }
-        }
-        return root;
-    }
-    
-    preorder(callback) {
-        const root = this.root;
-        callback(root.value);
-        if (root.left) root.left.preorder(callback);
-        if (root.right) root.right.preorder(callback);
-    }
-  
-    inorder(callback) {
-        const root = this.root;
-        if (root.left) root.left.inorder(callback);
-        callback(root.value);
-        if (root.right) root.right.inorder(callback);
-    }
-  
-    postorder(callback) {
-        const root = this.root;
-        if (root.left) root.left.postorder(callback);
-        if (root.right) root.right.postorder(callback);
-        callback(root.value);
-    }
-}
