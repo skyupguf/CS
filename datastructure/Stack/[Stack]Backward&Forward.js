d16e520d421ab7633b75dc7810f137c7c3990485
skyupguf <skyupguf@gmail.com>
2022-01-20 10:52:54 +0900

    Refact stack code

--- datastructure/Stack/[Stack]Backward&Forward.js
+++ datastructure/Stack/[Stack]Backward&Forward.js
@@ -1,33 +1,59 @@
-//  문제요약
-//  1. [prev=[], 현재페이지, next=[]] 형태의 웹브라우저가 존재한다.
-//  2. 길이가 100이하인 actions 배열에 알파벳은 새 페이지를, -1은 뒤로가기, 1은 앞으로 가기 이다. 
-//  3. 뒤로가기 일 경우 현재 페이지를 next에 넣고 prev에서 꺼낸다.
-//  4. 앞으로가기 일 경우 현재 페이지를 prev에 넣고 next에서 꺼낸다.
-//  5. 새로운 페이지로 이동시 현재 페이지를 prev에 넣고 next를 비운다.
-//  6. 둘다 비활성인 경우 push하지 않는다.
-//  7. 현재 페이지는 문자열 알파벳이 담긴 start 일 때, actions가 완료된 후 형태를 리턴하라.
-//  8. actions = ["B", "C", -1, "D", "A", -1, 1, -1, -1], start = "A", return [["A"], "B", ["A", "D"]]
-//  9. actions = ["B", -1, "B", "A", "C", -1, -1, "D", -1, 1, "E", -1, -1, 1], start = "A", return [ ["A", "B"], "D", ["E"]]
+//  문제
+/*
+    아래와 같은 조건들이 주어질 경우 웹 브라우저의 현재 페이지 이동 상태를 구하는 함수를 작성하라.
 
+    1. [prev, page, next] 형태의 웹브라우저가 존재한다.
+
+    2. 입력으로 웹브라우저 행동을 담은 배열 actions와 현재 페이지를 나타내는 start가 주어진다.
+        2-1. start는 현재 페이지를 문자열로 나타내며 알파벳으로 표시된다.
+        2-2. actions는 길이가 100이하인 배열로 새페이지인 알파벳, -1(뒤로가기), 1(앞으로가기)의 원소들로 구성되어 있다.
+    
+    3. 현재 페이지 start 부터 모든 actions가 수행될 때 다음과 같은 테스트 케이스의 결과가 도출된다.
+        3-1. actions = ["B", "C", -1, "D", "A", -1, 1, -1, -1], start = "A", return [["A"], "B", ["A", "D"]]
+        3-2. actions = ["B", -1, "B", "A", "C", -1, -1, "D", -1, 1, "E", -1, -1, 1], start = "A", return [ ["A", "B"], "D", ["E"]]
+        3-3. actions = [-1, 1], start = "A", return [[], "A", []]
+*/
+
+
+//  풀이
+/*
+    <접근방법>
+    브라우저의 페이지 이동 로직을 다음과 같이 정리할 수 있다.
+
+    1. 뒤로가기를 할 경우 현재 페이지가 next에 들어가고 prev의 페이지가 현재 페이지가 된다.
+    
+    2. 앞으로가를 할 경우 현재 페이지가 prev에 들어가고 next의 페이지가 현재 페이지가 된다.
+
+    3. 새로운 페이지로 이동 시 현재 페이지가 prev에 들어가고 next는 빈 상태가 된다.
+
+    4. prev와 next가 둘다 비어 비활성인 경우 현재 페이지는 그대로 유지된다.
+
+    위 이동로직을 통해 prev와 next의 페이지는 후입선출로 구현되어야 함을 알 수 있다.
+    따라서, 배열의 push, pop을 활용하여 stack자료구조로 구현한다.
+
+
+    <수도코드>
+    1. now, prev, next변수를 선언하고 각각 start, [], []을 할당한다.
+
+    2. actions배열을 루프하면서 각 원소에 맞는 행동을 조건으로 구현한다.
+        2-1. 원소가 -1이면 prev.length를 확인하고 next.push(now), now = prev.pop()을 한다.
+        2-2. 원소가 1이면 next.length를 확인하고 prev.push(now), now = next.pop()을 한다.
+        2-3. 원소가 문자면 prev.push(now), now = actions[i], next = []를 한다.
+    
+    3. 루프가 종료되면 [prev, now, next] 형태로 리턴한다.
+
+
+    <시간복잡도>
+    actions 원소를 한번 루프 하므로 O(N)
+*/
 //  코드
 const activateBrowser = (actions, start) => {
     let now = start, prev = [], next = [];
 
     actions.forEach(page => {
-        if(page === -1 && prev.length) next.push(now), now = prev.pop();
-        else if(page === 1 && next.length) prev.push(now), now = next.pop();
+        if(page === -1) prev.length ? (next.push(now), now = prev.pop()) : 0;
+        else if(page === 1) next.length ? (prev.push(now), now = next.pop()) : 0;
         else prev.push(now), next = [], now = page;
     });
     return [prev, now, next];
 }
-/*
-    풀이
-    1. 알파벳에서 알파벳으로 이동하면 prev.push(now) next = [] 가 된다.
-    2. -1이 나올 때 prev.length가 0이면 아무것도 일어나지 않는다, 반면 존재하면 prev.pop(), next.push(now)가 된다.
-    3. 1이 나올 때 next.lengthrk 0이면 아무것도 일어나지 않는다, 반면 존재하면 next.pop(), prev.push(now)가 된다.
-    4. 우선 3개의 변수를 prev, next, now를 선언하고 각각 [], start, []를 할당한다.
-    5. actions 배열을 루프하면서 1, 2, 3을 조건으로 분기시켜 작동 시킨다.
-
-    시간복잡도
-    actions 원소를 한번 루프 하므로 O(N)
-*/
