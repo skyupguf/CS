7c2b93931a4a9d7aa4d9371456feaf9ba25234c2
skyupguf <skyupguf@gmail.com>
2022-02-21 19:11:16 +0900

    Refact graph code

--- datastructure/Graph/[BFS&DFS]Linked_vertices.js
+++ datastructure/Graph/[BFS&DFS]Linked_vertices.js
@@ -13,8 +13,8 @@
     3. 추가로 인접행렬과 인접리스트로 BFS, DFS를 모두 구현해 본다.
 */
 
-/*  코드  */
-const countLinkedGraph = (edges) => {
+/*  인접행렬 코드  */
+const countLinkedMatrix = (edges) => {
     const size = edges.reduce((a, [v1, v2]) => a = Math.max(a, v1, v2), 0) + 1;
     const matrix = Array.from({length: size}, () => new Array(size).fill(0));
     edges.forEach(([v1, v2]) => {matrix[v1][v2] = matrix[v2][v1] = 1});
@@ -23,16 +23,34 @@ const countLinkedGraph = (edges) => {
     const visited = {};
     for (let row=0; row<matrix.length; row++) {
         if (!visited[row]) {
-            BFS(matrix, visited, row);
-            // DFS(matrix, visited, row)
+            matrixBfs(matrix, visited, row);
+            // matrixDfs(matrix, visited, row)
             graph++;
         }
     }
     return graph;
 }
 
-function BFS(matrix, visited, vertex) {
+function matrixBfs(matrix, visited, vertex) {
+    const queue = [vertex];
 
+    for (let i=0; i<queue.length; i++) {
+        visited[queue[i]] = true;
+        
+        let row = matrix[queue[i]];
+        for (let col=0; col<row.length; col++) {
+            if (row[col] && !visited[col]) queue.push(row[col]);
+        }
+    }
+}
+
+function matrixDfs(matrix, visited, vertex) {
+    visited[vertex] = true;
+
+    let row = matrix[vertex];
+    for (let col=0; col<row.length; col++) {
+        if (row[col] && !visited[col]) matrixDfs(matrix, visited, col);
+    }
 }
 /*
 //  접근방법  //
@@ -58,131 +76,75 @@ function BFS(matrix, visited, vertex) {
         2-5. 방문하지 않은 정점일 겨우 BFS 모듈함수로 만들어 matrix, row, visited를 인자로 전달해 구현한다.
 
     3. BFS를 활용해 인접행렬을 탐색한다.
-        3-1. 현재 정점과 인접한 
-        3-2. 
-        3-3. 
-        3-3. 
-
-
-    1. edges에 연결된 정점간의 간선을 표시하기 위해 인접행렬을 생성한다.
-        1-1. 인접행렬을 생성하기 위해 가장 큰 정점을 알아야 하므로 그래프의 전체 사이즈를 edges를 루프하여 구한다.
-        1-2. 가장 큰 정점 + 1이 전체 사이즈 이므로 해당 수 만큼 이차원 배열을 이중루프를 이용해 간선에 0을 채워 생성한다.
-        1-3. edges를 루프하면서 무향 그래프이므로 두 정점의 인덱스를 바꿔가면서 1을 할당해 인접행렬을 완성한다.
-
-    2. 인접행렬을 탐색하면서 간선이 연속으로 이어진 정점을 판별해 카운트를 해야한다.
-        2-1. for문으로 0부터 size까지 루프를 한다.
-        2-2. 한번 그래프를 탐색할 때 모든 정점을 탐색하기 때문에 간선으로 연결되어 있는 정점은 모두 탐색한다.
-        2-3. 따라서, visited에 방문처리가 되어있을 경우 정점을 중복 탐색해 버리기 때문에 탐색진입조건을 설정한다.
-        2-4. BFS/DFS 탐색을 위한 함수모듈을 호출하고 graph를 누적한다.
-
-    3. BFS함수를 함수모듈을 구현한다.
-        3-1. BFS탐색을 위해 queue를 src를 배열에 포함해 선언하고 시작정점을 방문처리 해준다.
-        3-2. while루프로 queue가 빈 배열이 될 때까지 루프하고 루프할 row가 될 정점을 queue에서 꺼내서 루프한다.
-        3-3. for루프에서 row에 간선이 존재하는 col을 큐에 push하고 해당 정점은 방문처리한다.
-        3-4. 간선이 이어진 정점은 전부 큐에 들어오게 되고 아닌 정점들만 남으므로 해당 그래프를 탐색할 수 있다.
+        3-1. 현재 정점과 간선이 존재하는 정점을 탐색할 queue를 선언해 인자로 받은 vertex를 삽입하고 visited에 방문처리한다.
+        3-2. 간선이 존재하지 않을 때 까지 탐색해야 하므로 queue를 루프하면서 matrix[vertex]의 간선을 체크해 queue에 삽입한다.
+        3-3. 이 때, visited에서 방문체크를 해 방문하지 않은 정점만 queue에 삽입한다.
+        3-4. 루프가 종료되면 객체인 visited에서 방문처리가 되기 때문에 리턴을 할 필요는 없다.
     
-    4. DFS함수를 함수모듈을 구현한다.
-        4-1. 탐색할 시작정점 row를 row변수에 할당하고 방문처리한다.
-        4-2. for문으로 루프하면서 col에 1이 존재하고 방문처리가 되어있지 않을 경우 재귀호출한다.
+    4. DFS를 활용해 인접행렬을 탐색한다.
+        4-1. BFS와 달리 간선이 존재할 경우 해당 정점으로 바로 이동해야하기 때문에 재귀호출을 이용한다.
+        4-2. 마찬가지로 visited의 결과만 있으면 되기 때문에 리턴값은 필요하지 않다.
+        4-3. row를 모두 루프해 더 이상 간선이 없으면 호출된 함수 이전으로 백트래킹하여 탐색을 계속한다.
     
-    5. 정점루프가 size와 같아지면 루프를 종료하고 찾은 그래프의 개수를 리턴한다.
+    5. 간선이 더 이상 이어지지 않을 경우 BFS 또는 DFS의 함수를 종료하고 graph를 누적한다.
 
     시간복잡도
     정점 탐색은 방문처리로 중복하지 않기 때문에, 인접행렬 생성으로 소요되는 O(N^2)이 최대 시간복잡도이다.
 */
-    let graph = 0
-    const visited = {};
 
-    const size = edges.reduce((a, [v1, v2]) => a = Math.max(a, v1, v2), 0) + 1;
-    const matrix = Array.from({length: size}, () => new Array(size).fill(0));
-    
-    edges.forEach(([v1, v2]) => { matrix[v1][v2] = matrix[v2][v1] = 1 });
-    for (let vertex=0; vertex<size; vertex++) {
-
-        if (!visited[vertex]) {
-            useBfs(matrix, vertex, visited), graph++;
-         // useDfs(matrix, vertex, visited), graph++;
-        }
-    }
-    return graph;
-
-function useBfs(matrix, src, visited) {
-    const queue = [src];
-    visited[src] = true;
-
-    while (queue.length) {
-        let row = matrix[queue.shift()];
-
-        for (let col=0; col<row.length; col++) {
-            if (row[col] && !visited[col]) {
-                queue.push(col), visited[col] = true;
-            }
-        }
-    }
-}
-
-function useDfs(matrix, src, visited) {
-    let row = matrix[src];
-    visited[src] = true;
-
-    for (let col=0; col<row.length; col++) {
-        if (row[col] && !visited[col]) {
-            useDfs(matrix, col, visited);
-        }
-    }
-}
-
-
-//  인접리스트 코드
-const countLinkedVertex = (edges) => {
-    let graph = 0;
-    const visited = {};
-
+/*  인접리스트 코드 */
+const countLinkedList = (edges) => {
     const list = {};
     const size = edges.reduce((a, [v1, v2]) => {
         list[v1] = {}, list[v2] = {};
         return a = Math.max(a, v1, v2);
     }, 0) + 1;
-
     edges.forEach(([v1, v2]) => {list[v1][v2] = list[v2][v1] = 1});
-    for (let node=0; node<size; node++) {
-        
-        if (!visited[node]) {
-            useBfs(list, node, visited), graph++;
-          //useDfs(list, node, visited), graph++;
+
+    let graph = 0;
+    const visited = {};
+    for (let vertex=0; vertex<size; vertex++) {
+        if (!visited[vertex]) {
+            listBfs(list, visited, vertex);
+            // listDfs(list, visited, vertex)
+            graph++;
         }
     }
     return graph;
 }
 
-function useBfs(list, src, visited) {
-    const queue = [src];
-    visited[src] = true;
+function listBfs(list, visited, vertex) {
+    const queue = [vertex];
 
-    while (queue.length) {
-        let vertex = queue.shift();
-
-        for (let adjVertex in list[vertex]) {
-            if (!visited[adjVertex]) {
-                queue.push(adjVertex), visited[adjVertex] = true;
-            }
+    for (let i=0; i<queue.length; i++) {
+        visited[queue[i]] = true;
+        
+        let adjList = list[queue[i]];
+        for (let node in adjList) {
+            if (!visited[node]) queue.push(node);
         }
     }
 }
 
-function useDfs(list, src, visited) {
-    visited[src] = true;
-
-    for (let adjVertex in list[src]) {
-        if (!visited[adjVertex]) {
-            useDfs(list, adjVertex, visited);
-        }
+function listDfs(list, visited, vertex) {
+    visited[vertex] = true;
+        
+    for (let node in list[vertex]) {
+        if (!visited[node]) listDfs(list, visited, node);
     }
 }
 /*
-    풀이
-    1. 인접행렬 코드와 크게 다르지 않으며, 행렬을 리스트로 구현하면 된다.
+//  접근방법  //
+    인접리스트는 탐색할 정점을 키로 인접한 정점들을 키-값으로 할당한 자료구조이다.
+    따라서 인접정점을 찾기 위해 객체를 루프하면서 해당 정점의 간선을 확인하는 방법을 제외하고 행렬코드와 크게 다르지 않다.
+
+
+//  수도코드  //
+    1. 인접리스트를 생성하고 edges를 루프하면서 인접리스트의 정점 키와 전체 크기를 구하고 간선을 할당한다.
+        1-1. list 변수를 선언하고 빈 객체를 할당한다.
+        1-2. edges를 루프하면서 각 정점들을 list의 키로 {}를 값으로 할당하고 전체 사이즈를 구한다.
+        1-3. edges를 루프하면서 탐색중인 정점과 인접한 정점을 키-값으로 list에 할당한다.
     
-    시간복잡도
-    하나의 정점 V와 인접한 정점들을 간선 E라고 하면 V + E만큼을 정확히 순회한다.
+    2. 리스트 자료구조를 사용하는 것 외에 모든 로직은 인접행렬과 동일하다.
+        2-1. 객체 자료구조에 맞게 루프와 조건문을 활용해 BFS, DFS 모듈함수를 구현한다.
 */
