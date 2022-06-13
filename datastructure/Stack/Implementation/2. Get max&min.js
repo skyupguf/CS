600cb9956f304e8920b64c944a4c84fe07361a5b
skyupguf <skyupguf@gmail.com>
2022-06-13 19:45:26 +0900

    Modify implementation stack code

--- datastructure/Stack/Implementation/2. Get max&min.js
+++ datastructure/Stack/Implementation/2. Get max&min.js
@@ -18,6 +18,7 @@ class Stack {
     }
     
     push (data) {
+        if (!Number.isInteger(data)) return;
         if (this.top >= this.size) return new Error('stack overflow');
         this.stack[this.top] = data;
         
@@ -66,15 +67,16 @@ class Stack {
     1. 기존 스택의 구현에 min과 max 변수를 생성하고 빈 배열을 할당한다.
         1-1. min과 max는 Array.prototype을 활용해 불필요한 변수를 늘리지 않는다.
 
-    2. push (data)는 기존 스택과 동일하게 구현하고 min과 max 로직을 추가한다.
-        2-1. stack에 top누적을 제외한 추가 로직을 우선 구현한다. 
-        2-2. stack이 비어있는 경우 min과 max의 값이 동일하기 때문에 분기하여 동일한 경우 각 배열에 동일한 data를 삽입한다.
-        2-3. stack에 데이터가 존재할 경우 data가 min의 마지막 값 미만, max의 마지막 값 초과일 경우 push(data)를 한다.
-        2-4. top의 경우 2-2의 stack이 비어있을 경우의 조건을 만들기 위해 top을 가장 마지막에 누적한다.
+    2. push (data)는 기존 스택에 min과 max 로직을 추가한다.
+        2-1. 정수를 다루는 스택이기 때문에 최우선적으로 data가 정수인지 판별해서 예외처리 한다.
+        2-2. 정수 판별 후 오버플로우를 체크하고 스택에 data를 추가한다.
+        2-3. stack이 비어있는 경우 min과 max의 값이 동일하기 때문에 각 배열에 동일한 data를 삽입한다.
+        2-4. stack에 데이터가 존재할 경우 data가 min의 마지막 값 미만, max의 마지막 값 초과일 경우 각 배열에 삽입한다.
+        2-5. 모든 작업이 완료되면 top을 누적한다.
     
-    3. pop ()의 경우도 마찬가지로 기존 로직을 먼저 구현후 리턴전에 min, max로직을 완성한다.
+    3. pop ()의 경우도 마찬가지로 기존 로직을 먼저 구현 후 리턴전에 min, max로직을 완성한다.
         3-1. stack에서 추출할 값을 임시 변수 temp에 할당하고 temp를 리턴하기전의 모든 로직을 완성시킨다.
-        3-2. temp값과 동일한 값이 min과 max의 마지막 요소값과 동일한지 각각 확인하고 제거한다.
+        3-2. temp값과 min, max배열의 마지막 요소값이 동일한 경우 제거한다.
         3-3. temp값을 리턴한다.
 
     4. top과 peek 메서드는 변동사항 없이 동일하다.
@@ -83,18 +85,18 @@ class Stack {
 
 
 //  복잡도  //
-    각 메서드의 작동은 모두 스택로직을 기반으로 하기 때문에 O(1)의 시간에 모두 작동된다.
+    각 메서드는 모두 스택로직을 기반으로 하기 때문에 O(1)의 시간에 모두 작동된다.
     하지만, min과 max의 추가 배열이 따로 필요하기 때문에, 데이터를 2중으로 저장하게 되므로 O(N)의 추가공간이 필요하다.
 */
 
 /*
 //  리팩토링  //
-    추가공간 없이 최대, 최소값을 가져오는 스택을 설계해 보자.
+    추가공간 없이 최대, 최소값을 탐색할 수 있는 스택을 설계해 보자.
 */
 
 /*  코드  */
 class Stack {
-    constructor (size) {//기존 스택 멤버변수에서 min과 max변수를 상수단위로 할당하도록 한다.
+    constructor (size) {
         this.stack = {};
         this.min;
         this.max;
@@ -103,62 +105,88 @@ class Stack {
     }
     
     push (data) {
+        if (!Number.isInteger(data)) return;
         if (this.top >= this.size) return new Error('stack overflow');
-        //우선 stack이 비었을 때 stack삽입값, 최대, 최소값은 동일하다
+        
         if (this.top === 0) this.stack[this.top] = data, this.min = data, this.max = data;
-        else {//값을 추가하는 3가지 경우가 존재한다.
-            //2. 최대값에 해당되는 경우
-            //3. 모두 해당되지 않는 경우
-            if (this.min > data) this.stack[this.top] = data - this.min, this.min = data;//1. 최소값에 해당되는 경우 : stack에 data - min을 삽입하고 min을 data로 갱신한다.
-            else if (this.max < data) this.stack[this.top] = data + this.max, this.max = data;//2. 최대값에 해당되는 경우 : stack에 data + max을 삽입하고 max를 data로 갱신한다.
-            else this.stack[this.top] = data;//3. 둘다 해당되지 않으면 stack에 그냥 삽입한다.
+        else {
+            if (this.min > data) this.stack[this.top] = data - this.min, this.min = data;
+            else if (this.max < data) this.stack[this.top] = data + this.max, this.max = data;
+            else this.stack[this.top] = data;
         }
         this.top++;
     }
 
     pop () {
         if (this.top <= 0) return new Error('stack underflow');
-        const temp = this.stack[this.top-1];
-        //추출하는 값과 min, max를 모두 비교하여 원본 값으로 복원하여 추출하고 최소, 최대값을 이전으로 갱신해야 한다.
-        if (this.min > temp) //추출값이 min보다 작을 경우 최소값에 해당되므로 temp를 최소 값으로 갱신하고 최소값은 min - temp
-        
+
+        let temp = this.stack[this.top-1];
+        if (this.min > temp) [this.min, temp] = [this.min-temp, this.min]; 
+        else if (this.max < temp) [this.max, temp] = [temp-this.max, this.max];
+
         delete this.stack[this.top-1], this.top--;
+        return temp;
     }
     
     last () {
-        return this.stack[this.top-1];
-    }
-
-    peek (index) {
-        if (!Number.isInteger(index) || index < 0 || index >= this.top) return;
-        return this.stack[index];
+        let temp = this.stack[this.top-1];
+        if (this.min > temp) temp = this.min; 
+        else if (this.max < temp) temp = this.max;
+        return temp;
     }
     
     getMin () {
-        return 
+        return this.min;
     }
 
     getMax () {
-        return 
+        return this.max;
     }
 }
 /*
 //  접근방법  //
-    앞선 코드에선 이전 최대, 최소값을 기억하기 위해 배열을 사용하였는데 추가공간 O(N)이 발생하게 된다.
-    하지만 간단한 수식을 활용하면 stack의 저장공간으로 기억이 가능하기 때문에 최소, 최대값을 상수단위로 할당할 수 있다.
-    stack을 기억공간으로 두고 최소값은 삽입값 - 현재 최소값, 최대값은 삽입값 + 현재 최대값 으로 stack에 기억시켜 둔다.    
-    이렇게 할 경우 최대, 최소값이 아닌 경우는 최소, 최대값과 비교 후 확인하여 그냥 추출하면 되고
-    최소, 최대값보다 작거나 큰 경우 값을 복원하여 추출하면 된다.
+    추가공간을 사용하지 않으려면 이전값을 기억하는 유일한 공간은 stack이므로 이를 활용한다.
+    최소값과 최대값을 할당할 min, max 변수는 상수를 할당하도록 하고 값을 기억하고 추출할 때 수식을 활용한다.
+    
+    삽입시
+    1. 삽입값이 최소값인 경우 (삽입값 - 현재 최소값)을 stack에 삽입하고 최소값은 삽입값으로 갱신한다.
+    2. 삽입값이 최대값인 경우 (삽입값 + 현재 최대값)을 stack에 삽입하고 최대값은 삽입값으로 갱신한다.
+    3. 해당되지 않는 경우 stack에 값을 삽입한다.
+    
+    추출시
+    1. 최소값 > 추출값인 경우 (최소값 - 추출값)을 최소값으로 갱신하고 스택에서 추출값을 제거, 최소값을 리턴한다.
+    2. 최대값 < 추출값인 경우 (추출값 - 최대값)을 최데값으로 갱신하고 스택에서 추출값을 제거, 최소값을 리턴한다.
+    3. 해당되지 않는 경우 stack에서 값을 추출한다.
+
+    즉, 스택에 변형해서 기록하고 추출할 때 복원해서 추출한다.
 
 
 //  수도코드  //
-    1. 
+    1. 기존 스택 멤버변수에서 min과 max변수에 상수를 할당하도록 선언만 해둔다.
 
-    2. 
+    2. push메서드에서 stack의 데이터 존재여부, 최대, 최소값의 갱신여부로 각각 분기한다.
+        2-1. 스택이 비어있을 경우 stack삽입값, 최대, 최소값을 동일하게 할당한다.
+        2-2. 스택에 데이터가 존재할 경우에 다시 3가지의 경우로 분기한다.
+        2-3. min > data의 경우 (data - min)을 stack에 삽입하고 min을 data로 갱신한다.
+        2-4. max < data의 경우 (data + max)을 stack에 삽입하고 max를 data로 갱신한다.
+        2-5. 둘다 해당되지 않으면 stack에 그냥 삽입한다.
+        2-6. 모든 작업이 완료됐으면 top을 1누적한다.
     
-    3. 
+    3. pop메서드는 추출되는 3가지 경우를 고려한다.
+        3-1. 임시변수 temp를 선언하고 추출할 스택값을 할당한다.
+        3-2. min > temp일 경우 (min - temp)로 이전 최소값을 복원하여 현재 min에 할당하고 temp엔 min을 할당한다.
+        3-3. max < temp일 경우 (temp - max)로 이전 최대값을 복원하여 현재 max와 temp를 분해할당한다.
+        3-4. 둘 다 해당되지 않는 경우 temp에 할당한 추출값을 그대로 리턴하면 된다.
+        3-5. stack에 추출할 값을 제거하고 top을 1차감한 후 temp를 리턴한다.
+
+    4. last메서드의 경우 최대, 최소값일 경우 복원 값을 리턴해야 한다.
+        4-1. temp임시변수를 선언하고 stack의 마지막 값을 할당한다.
+        4-2. temp가 최소값 보다 작으면 min을 최대값 보다 크면 max를 temp에 할당한다.
+        4-3. temp를 리턴한다.
+
+    5. peek 메서드의 경우 시간 복잡도가 O(N)이 소요될 수 있으므로 꼭 필요하지 않으면 추가하지 않는다.
 
 
 //  복잡도  //
-    
+    시간복잡도는 기존 스택로직을 그대로 유지하면서 추가 공간 없이 스택을 활용할 수 있다.
 */
