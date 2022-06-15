11b53b332484a9057d75839b34d2910034e8cd41
skyupguf <skyupguf@gmail.com>
2022-06-15 22:35:36 +0900

    Add implementation stack code

---
+++ datastructure/Stack/Implementation/3. Two stacks.js
@@ -0,0 +1,29 @@
+/*
+//  문제요약  //
+    하나의 공간에 두 개의 스택을 각각 사용할 수 있도록 설계하라.
+    
+    1. 공간을 가능한 효율적으로 운영할 수 있도록 설계해야 한다.
+*/
+
+/*  코드  */
+class Two_stack {
+    constructor (size) {
+        this.stack = {};
+        this.top = 0;
+        this.size = size || Number.MAX_SAFE_INTEGER;
+    }
+
+    
+}
+/*
+//  접근방법  //
+    
+    
+
+//  수도코드  //
+    
+    
+
+//  시간복잡도  //
+    
+*/
