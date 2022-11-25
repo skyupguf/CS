3ee60adf3f3c3b8711274bd397d7a3106ed8be40
skyupguf <skyupguf@gmail.com>
2022-11-25 18:35:20 +0900

    add new practice page

---
+++ Algorithm/Sort/src/Main.java
@@ -0,0 +1,47 @@
+/**
+ * 수 정렬하기 3
+ *  첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다.
+ *  둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+ *  입력 받은 수들을 O(N)의 연산안에 오름차순 정렬하여 한줄에 하나씩 출력하라.
+ */
+
+/*
+* 정렬 알고리즘을 O(N)으로 수행하는 방법은 계수정렬이 유일하다.
+* 값을 비교해서 교환하는 방식이 아니라 한 번의 루프로 정렬을 하기 때문이다.
+* 다만 한정 된 수의 정렬에만 유리한 이유가 추가 공간이 필요하기 때문이다.
+* 입력받은 수만큼의 배열을 생성하고 해당 수를 하나씩 루프하면서 수를 생성한 인덱스에 하나씩 카운트한다.
+* 카운트가 완료되면 배열을 루프하면서 카운트된 수만큼 인덱스를 출력하면 된다.
+* 입력받은 수 만큼의 배열 공간을 추가해야 하므로 당연히 공간을 더 많이 쓰게 된다.
+* */
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    void solution(int delay, int capacity, int[] times) {
+        Queue<Integer> queue = new LinkedList();
+        int time = 0;
+        int i = 0;
+        int cnt = 0;
+        int inputSize = 0;
+
+        while (i != times.length) {
+            if (queue.peek() == time) {
+                queue.remove();
+            }
+            if (queue.size() < capacity && times[i] != 0) {
+                inputSize += times[i];
+                queue.add(inputSize);
+            } else if (queue.size() >= capacity && times[i] != 0) {
+                cnt++;
+            }
+            i++;
+            time++;
+        }
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Memo.java
@@ -0,0 +1,8 @@
+// 코딩 연습장
+
+public class Memo {
+
+    public static void main(String[] args) {
+
+    }
+}
