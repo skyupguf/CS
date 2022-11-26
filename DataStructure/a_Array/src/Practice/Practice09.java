fd006c74a08170ef9601ec2917f8805b16cb858d
skyupguf <skyupguf@gmail.com>
2022-11-26 20:42:15 +0900

    Add Array practice

---
+++ DataStructure/a_Array/src/Practice/Practice09.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class Practice09 {
+
+    public static void solution(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        solution(matrix);
+
+        System.out.println();
+        matrix = new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        solution(matrix);
+    }
+}
