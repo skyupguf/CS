40dca56ab5e8572c1c6f042b8bbc2c41cde232a6
skyupguf <skyupguf@gmail.com>
2022-12-15 01:11:21 +0900

    Save current state not completed

--- Algorithm/Sort/src/Test/S_24092.java
+++ Algorithm/Sort/src/Test/S_24092.java
@@ -58,15 +58,16 @@ public class S_24092 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
         arrayA = new long[N];
         arrayB = new long[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) { arrayA[i] = Integer.parseInt(st.nextToken()); }
+
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) { arrayB[i] = Integer.parseInt(st.nextToken()); }
-
 
         if (Arrays.equals(arrayA, arrayB)) {
             bw.write('1');
