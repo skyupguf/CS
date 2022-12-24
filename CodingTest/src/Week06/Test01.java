81d7b6e3861a603b720ee0285cec0b166a3b6fb5
skyupguf <skyupguf@gmail.com>
2022-12-24 11:51:12 +0900

    Solve week06_Test01

--- CodingTest/src/Week06/Test01.java
+++ CodingTest/src/Week06/Test01.java
@@ -19,39 +19,64 @@ package Week06;
     {4, 8}
 * */
 
-
 import java.util.Arrays;
 
 public class Test01 {
-
+    // 첫 코드
     public static int[] solution (int[] values) {
+
         int[] result = new int[2];
+
         // 두개의 포인터를 사용해 시작 구간부터 더 이상 커지지 않는 구간까지 비교한다.
-        int p1 = 0;
-        int p2 = 1;
-        int max = values[p1];
-        int interval = 0;
-        int count = 0;
+        int p1 = 0, p2 = 1;
+        int maxInterval = 0;
+        int intervalCnt = 0;
+        int intervalMaxNum = values[p1];
 
         while (p2 < values.length) {
-
-            while (p2 < values.length && max < values[p2]) {
-                max = values[p2++];
-                ++count;
+            // p2가 values 를 초과하면 index 에러가 발생한다, 값의 증가가 끝날 때 까지 intervalMaxNum와 intervalCnt 갱신
+            while (p2 < values.length && intervalMaxNum < values[p2]) {
+                intervalMaxNum = values[p2++];
+                ++intervalCnt;
             }
-
-            if (interval < count) {
+            // 이전 저장한 구간과 비교 후 클 경우 구간 저장, 인덱스를 결과에 할당, p2는 1 증가된 상태이미르 1차감해서 할당
+            if (maxInterval < intervalCnt) {
                 result[0] = p1;
                 result[1] = --p2;
-                interval = count;
+                maxInterval = intervalCnt;
             }
-
+            // p2가 values 를 초과할 경우 탐색이 종료된 상태이므로 코드를 실행하지 못하도록 한다.
             if (p2 < values.length) {
-                count = 0;
+                intervalCnt = 0;
                 p1 = p2;
                 ++p2;
-                max = values[p1];
+                intervalMaxNum = values[p1];
             }
+        }
+        return result;
+    }
+
+    // 리팩토링
+    public static int[] solution2 (int[] values) {
+        int left = 0;
+        int right = 0;
+        int[] result = {left, right};
+
+        while (left < values.length-1) {
+
+            for (int i = left; i < values.length-1; i++) {
+
+                if (values[i] >= values[i+1]) {
+                    right = i;
+                    break;
+                }
+                right = i + 1;
+            }
+
+            if (result[1] - result[0] < right - left) {
+                result = new int[] {left, right};
+            }
+            left = right + 1;
         }
         return result;
     }
@@ -59,17 +84,22 @@ public class Test01 {
     public static void main(String[] args) {
         int[] arr = {103, 152, 124, 165, 152, 154, 159, 160, 200, 195, 205, 206, 204, 189, 156};
         System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution2(arr)));
 
         arr = new int[] {5};
         System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution2(arr)));
 
         arr = new int[] {100, 80, 60, 40, 20, 0};
         System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution2(arr)));
 
         arr = new int[] {10, 20, 30, 40, 50, 60};
         System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution2(arr)));
 
         arr = new int[] {100, 170, 120, 130, 105, 113};
         System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution2(arr)));
     }
 }
