af10f92f55c28fde487ddf85cecf1e2e110d0520
skyupguf <skyupguf@gmail.com>
2023-01-10 21:56:17 +0900

    Learn shortest paths algorithm concept

---
+++ Algorithm/ShortestPaths/src/Concept/Dijkstra.java
@@ -0,0 +1,22 @@
+package Concept;
+// 알고리즘 - 최단 경로 알고리즘
+// 다익스트라 구현
+
+public class Dijkstra {
+
+    public static void dijkstra(int v, int[][] data, int start) {
+
+    }
+
+
+    public static void priorityDijkstra(int v, int[][] data, int start) {
+
+    }
+
+
+    public static void main(String[] args) {
+        // Test code
+        int[][] data = {{1, 2, 2}, {1, 3, 3}, {2, 3, 4}, {2, 4, 5}, {3, 4, 6}, {5, 1, 1}};
+        dijkstra(5, data, 1);
+    }
+}
